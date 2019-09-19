c23e2720906e06f335ec9b392d273cee62a7fd52
trung-doan <trung-doan@cybozu.vn>
2019-09-19 14:48:51 +0700

    init sample code

---
+++ sample/src/test/java/sample/AppTest.java
@@ -0,0 +1,15 @@
+package sample;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertEquals(1, 1);
+    }
+}
